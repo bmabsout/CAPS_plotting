4394ff006c64b0af6840ee5d0e145c8297ff95f1
Bassel El Mabsout <bmabsout@bu.edu>
2021-03-24 15:53:33 -0400

    updated reality gap script

--- Plot_reality_gap.py
+++ Plot_reality_gap.py
@@ -11,12 +11,20 @@ print(ys.shape)
 ys = ys[:, :1000]
 y = np.mean(ys, axis=0)
 y_std = np.std(ys, axis=0)
-x = windows_with_errors[0]["t"][:len(y)]
-fig, ax = plt.subplots(figsize=(5,4))
-legend_stuff = utils.plot_with_std(ax, x, y, y_std)
+t = windows_with_errors[0]["t"][:len(y)]
+dt = t[1:] - t[:-1]
+ys_d = (ys[:, 1:] - ys[:, :-1])/dt
+print(np.shape(ys_d))
+print("change in rate", np.mean(ys_d))
+fig, ax = plt.subplots(2, figsize=(5,4))
+legend_stuff = utils.plot_with_std(ax[0], t, y, y_std)
 print(y_std[1])
 print(y[1])
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("APG (degrees/s)")
-ax.legend(legend_stuff, ("std", "mean"))
+ax[0].set_xlabel("Time (s)")
+ax[0].set_ylabel("APG (degrees/s)")
+ax[0].legend(legend_stuff, ("std", "mean"))
+ax[0].set_yscale('symlog')
+ax[1].set_yscale('symlog')
+utils.plot_with_std(ax[1], t[0:-1], np.mean(ys_d, axis=0), np.std(ys_d, axis=0))
+# ax[1].plot(t[:-1], y_d)
 plt.show()
